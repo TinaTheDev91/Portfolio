1e61719c07b1cdc65799041149b51c832cdbdb05
TinaTheDev91 <122223572+TinaTheDev91@users.noreply.github.com>
2023-06-14 22:57:38 -0400

    implemented tailwind

--- portfolio/src/components/pages/Contact.js
+++ portfolio/src/components/pages/Contact.js
@@ -4,9 +4,24 @@ export default function Contact() {
   return (
     <div>
       <h1>Contact Form</h1>
-      <p>
-        Contact Form 
-      </p>
+      <form className="form">
+        <input
+          name="name"
+          type="text"
+          placeholder="name"
+        />
+        <input
+          name="email"
+          type="email"
+          placeholder="email"
+        />
+        <input
+          name="message"
+          type="text"
+          placeholder="message"
+        />
+        <button type="button">Submit</button>
+      </form>
     </div>
   );
 }
