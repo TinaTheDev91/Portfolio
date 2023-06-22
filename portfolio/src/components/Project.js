8fdf8cf66d75d865da8c293399e360dcbce76691
TinaTheDev91 <122223572+TinaTheDev91@users.noreply.github.com>
2023-06-22 19:44:24 -0400

    updated links to open in a new tab

--- portfolio/src/components/Project.js
+++ portfolio/src/components/Project.js
@@ -10,12 +10,12 @@ export default function Project (props) {
     return (
         <div className="project-list flex">
             <div className="projectCard card w-96 glass">
-            <figure><img src={require(`./assets/project-ss/${image}.jpg`)} alt="car!"/></figure>
+            <figure><img src={require(`./assets/project-ss/${image}.jpg`)} alt="application screenshot"/></figure>
             <div className="card-body">
                 <h2 className="card-title">{name}</h2>
                 <p>{info}</p>
                 <div className="card-actions justify-end">
-                    <a href={`${link}`}>
+                    <a href={`${link}`} target="_blank" rel="noopener noreferrer">
                         <button className="btn btn-primary">Visit here</button>
                     </a>
                 </div>
