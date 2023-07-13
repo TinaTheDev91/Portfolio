06a00721c56352d1878d992fbc1f9dbc3f9fe4c4
TinaTheDev91 <122223572+TinaTheDev91@users.noreply.github.com>
2023-07-13 19:43:41 -0400

    added ocdetour to portfolio

--- portfolio/src/components/ProjectData.js
+++ portfolio/src/components/ProjectData.js
@@ -1,28 +1,37 @@
 // var LaborHubImg = './assets/project-ss/LaborHub.jpg'
 
-export const projectData = [{ 
-    key: 1, 
+export const projectData = [
+{
+    key: 5,
+    name: 'OCDetour',
+    link: 'https://ocdetour-4111fd1eff32.herokuapp.com/',
+    info: 'OCDetour is an app to help delay compulsions for people with OCD. Leveraging research published in the journal of Behavioral Analysis, OCDetour utilizes the concept of delay discounting.',
+    image: 'ocdetour'
+},
+    
+    { 
+    key: 4, 
     name: 'LaborHub', 
     link: 'https://laborhub.herokuapp.com/', 
     info: 'LaborHub is an app for managing labor unions that allows users to share their wages, demographics data, and check if dues are paid.',
     image: 'laborhub'
 },
 {
-    key: 2, 
+    key: 3, 
     name: 'Weather Dashboard', 
     link: 'https://tinathedev91.github.io/Weather-Dashboard/', 
     info: 'Weather Dashboard is an app with 3rd party API integration that uses the Bootstrap Framework.',
     image: 'weatherdashboard'
 },
 {
-    key: 3,
+    key: 2,
     name: 'Work Day Scheduler',
     link: 'https://tinathedev91.github.io/Work-Day-Scheduler/',
     info: 'Work Day Scheduler is an app using the Day.js and jQuery libraries, as well as local storage for saving data entries.',
     image: 'workdayscheduler'
 },
 {
-    key: 4,
+    key: 1,
     name: 'Note Taker App',
     link: 'https://note-taker-tlc.herokuapp.com/',
     info: 'Note taking application using backend storage with the user adding and removing data from the application\'s json file using Express.js',
